6adcf85f8f99f7068666f8447c7aecbef42799c5
Dextr Lumbr <info@timbrtech.com>
2020-11-12 15:08:05 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb+srv://videojenkins:Hoopshype1@cluster0.cxpup.mongodb.net/Project 0?retryWrites=true&w=majority', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('sbs-videos')
+    const quotesCollection = db.collection('videoInfo')
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+    
+  })
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// All your handlers here...
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // res.redirect('/')
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
